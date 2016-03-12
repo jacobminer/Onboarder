2168f9073dc1b9efb38d8e58e5c13958d2caf13b
jrejaud <jrejaud@gmail.com>
2016-03-11 18:54:59 -0800

    int commit

---
+++ onboarder/src/main/java/com/jrejaud/onboarder/OnboardingActivity.java
@@ -0,0 +1,134 @@
+package com.jrejaud.onboarder;
+
+import android.os.Bundle;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.github.jrejaud.viewpagerindicator2.CirclePageIndicator;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class OnboardingActivity extends AppCompatActivity {
+    private final static String BACKGROUND_IMAGE_RES_ID = "BACKGROUND_IMAGE_RES_ID";
+    private @DrawableRes int backgroundImageResId;
+    private final static String BACKGROUND_COLOR_RES_ID = "BACKGROUND_COLOR_RES_ID";
+    private @ColorRes int backgroundColorResId;
+
+    private final static String ONBOARDING_FRAGMENT_LIST = "ONBOARDING_FRAGMENT_LIST";
+    private List<OnboardingFragment> onboardingFragmentList; //TODO make this an array instead?
+
+    public final static String SWIPING_ENABLED = "SWIPING_ENABLED";
+
+    /** Whether the user can swipe the screen left or right to move between fragments */
+    private boolean swipingEnabled;
+
+    /** Whether the pagination dots at the bottom of the activity should be down of not */
+    private final static String HIDE_DOT_PAGINATION = "HIDE_DOT_PAGINATION";
+    private boolean hideDotPagination;
+
+    //region Static Factory Methods
+    public static Bundle newBundleImageBackground(@DrawableRes int backgroundImageResId, @NonNull List<OnboardingFragment> onboardingFragmentList) {
+        Bundle bundle = new Bundle();
+        bundle.putInt(BACKGROUND_IMAGE_RES_ID, backgroundImageResId);
+
+        //Lists are serializable in Java
+        bundle.putSerializable(ONBOARDING_FRAGMENT_LIST, (Serializable) onboardingFragmentList);
+        return bundle;
+    }
+
+    public static Bundle newBundleColorBackground(@ColorRes int backgroundColorResId, @NonNull List<OnboardingFragment> onboardingFragmentList) {
+        Bundle bundle = new Bundle();
+        bundle.putInt(BACKGROUND_COLOR_RES_ID, backgroundColorResId);
+
+        //Lists are serializable in Java
+        bundle.putSerializable(ONBOARDING_FRAGMENT_LIST, (Serializable) onboardingFragmentList);
+        return bundle;
+    }
+
+    public static Bundle newBundleImageBackground(@DrawableRes int backgroundImageResId, boolean swipingEnabled, boolean hideDotPagination, @NonNull List<OnboardingFragment> onboardingFragmentList) {
+        Bundle bundle = new Bundle();
+        bundle.putInt(BACKGROUND_IMAGE_RES_ID, backgroundImageResId);
+        bundle.putBoolean(SWIPING_ENABLED, swipingEnabled);
+        bundle.putBoolean(HIDE_DOT_PAGINATION, hideDotPagination);
+
+        //Lists are serializable in Java
+        bundle.putSerializable(ONBOARDING_FRAGMENT_LIST, (Serializable) onboardingFragmentList);
+        return bundle;
+    }
+
+    public static Bundle newBundleColorBackground(@ColorRes int backgroundColorResId, boolean swipingEnabled, boolean hideDotPagination, @NonNull List<OnboardingFragment> onboardingFragmentList) {
+        Bundle bundle = new Bundle();
+        bundle.putInt(BACKGROUND_COLOR_RES_ID, backgroundColorResId);
+        bundle.putBoolean(SWIPING_ENABLED, swipingEnabled);
+        bundle.putBoolean(HIDE_DOT_PAGINATION, hideDotPagination);
+
+        //Lists are serializable in Java
+        bundle.putSerializable(ONBOARDING_FRAGMENT_LIST, (Serializable) onboardingFragmentList);
+        return bundle;
+    }
+    //endregion
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        //Extract Data from the Bundle
+        Bundle bundle = getIntent().getExtras();
+
+        //Todo need to implement a non-swiping version
+        swipingEnabled = bundle.getBoolean(SWIPING_ENABLED,true);
+        hideDotPagination = bundle.getBoolean(HIDE_DOT_PAGINATION, false);
+        backgroundImageResId = bundle.getInt(BACKGROUND_IMAGE_RES_ID, -1); //-1 means that no image was passed
+        backgroundColorResId = bundle.getInt(BACKGROUND_COLOR_RES_ID,-1);
+        onboardingFragmentList = (List<OnboardingFragment>) bundle.getSerializable(ONBOARDING_FRAGMENT_LIST);
+
+        //Set the view pager
+        //TODO Implement a non-swiping version
+        ViewPager viewPager = (ViewPager) findViewById(R.id.onboarding_viewpager);
+        viewPager.setAdapter(new OnboardingFragmentPagerAdapter(getSupportFragmentManager()));
+
+        //Set the dot pagination
+        CirclePageIndicator circlePageIndicator = (CirclePageIndicator)findViewById(R.id.onboadring_page_indicator);
+        if (!hideDotPagination) {
+            circlePageIndicator.setViewPager(viewPager);
+        } else {
+            circlePageIndicator.setVisibility(View.GONE);
+        }
+
+        //Set the background Image or Color
+        ImageView onboardingBackground = (ImageView) findViewById(R.id.onboarding_background_image);
+        if (backgroundImageResId!=-1) {
+            onboardingBackground.setImageResource(backgroundImageResId);
+        } else if (backgroundColorResId!=-1) {
+            getWindow().getDecorView().setBackgroundColor(getResources().getColor(backgroundColorResId));
+        }
+
+    }
+
+    private class OnboardingFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        public OnboardingFragmentPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return onboardingFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return onboardingFragmentList.size();
+        }
+    }
+}
---
+++ onboarder/src/main/java/com/jrejaud/onboarder/OnboardingFragment.java
@@ -0,0 +1,162 @@
+package com.jrejaud.onboarder;
+
+import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.Serializable;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link OnboardingFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link OnboardingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OnboardingFragment extends Fragment implements Serializable {
+
+    // the fragment initialization parameters
+    private static final String TITLE = "TITLE";
+    private static final String BODY_TEXT = "BODY_TEXT";
+    private static final String IMAGE_RESOURCE_ID = "IMAGE_RESOURCE_ID";
+
+    /** The title which is displayed at the top of the fragment */
+    private String title;
+
+    /** The body text which is displayed in the middle of the fragment */
+    private String bodyText;
+
+    /** The image resource which is displayed in the middle of the fragment, above the text */
+    private @DrawableRes int imageResId;
+
+//    private OnFragmentInteractionListener mListener;
+
+    public OnboardingFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param title The title which is displayed at the top of the fragment.
+     * @param bodyText The body text which is displayed in the middle of the fragment.
+     * @param imageResId The image resource which is displayed in the middle of the fragment, above the text
+     * @return A new instance of fragment OnboardingFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static OnboardingFragment newInstance(String title, String bodyText,@DrawableRes int imageResId) {
+        OnboardingFragment fragment = new OnboardingFragment();
+        Bundle args = new Bundle();
+        args.putString(TITLE, title);
+        args.putString(BODY_TEXT, bodyText);
+        args.putInt(IMAGE_RESOURCE_ID, imageResId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public static OnboardingFragment newInstance(String title, String bodyText) {
+        OnboardingFragment fragment = new OnboardingFragment();
+        Bundle args = new Bundle();
+        args.putString(TITLE, title);
+        args.putString(BODY_TEXT, bodyText);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            title = getArguments().getString(TITLE);
+            bodyText = getArguments().getString(BODY_TEXT);
+            imageResId = getArguments().getInt(IMAGE_RESOURCE_ID);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_onboarding, container, false);
+
+        // Inflate the layout for this fragment
+        Bundle bundle = getArguments();
+
+        title = bundle.getString(TITLE,null);
+        bodyText = bundle.getString(BODY_TEXT, null);
+        imageResId = bundle.getInt(IMAGE_RESOURCE_ID,-1);
+
+        TextView titleTextView = (TextView) view.findViewById(R.id.onboarding_fragment_title);
+        TextView bodyTextView = (TextView) view.findViewById(R.id.onboarding_fragment_body_text);
+        ImageView imageView = (ImageView) view.findViewById(R.id.onboarding_fragment_image);
+
+        //Set the title
+        if (title!=null) {
+            titleTextView.setText(title);
+        } else {
+            titleTextView.setVisibility(View.GONE);
+        }
+
+        //Set the body text
+        if (bodyText!=null) {
+            bodyTextView.setText(bodyText);
+        } else {
+            bodyTextView.setVisibility(View.GONE);
+        }
+
+        //Set the image
+        if (imageResId!=-1) {
+            imageView.setImageResource(imageResId);
+        } else {
+            imageView.setVisibility(View.GONE);
+        }
+
+        return view;
+    }
+
+//    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
+//
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+//
+//    /**
+//     * This interface must be implemented by activities that contain this
+//     * fragment to allow an interaction in this fragment to be communicated
+//     * to the activity and potentially other fragments contained in that
+//     * activity.
+//     * <p/>
+//     * See the Android Training lesson <a href=
+//     * "http://developer.android.com/training/basics/fragments/communicating.html"
+//     * >Communicating with Other Fragments</a> for more information.
+//     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+//    }
+}
